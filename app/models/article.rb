a92b195d4cec7d2d20727cbfd1e57e44f78b954e
Mohammed Abdelrasoul <mohammed.abdelrasoul@gmail.com>
2016-01-23 14:59:31 +0200

    create user Controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true , length: {minmum: 3, maximum: 60}
   validates :description, presence: true, length: {minmum: 10, maximum: 400}
 end
