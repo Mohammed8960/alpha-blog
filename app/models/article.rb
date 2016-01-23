9864570bbf598cc56d1e94fab4afccad82851ee0
Mohammed Abdelrasoul <mohammed.abdelrasoul@gmail.com>
2016-01-23 18:50:10 +0200

    add category controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true , length: {minmum: 3, maximum: 60}
   validates :description, presence: true, length: {minmum: 10, maximum: 400}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
